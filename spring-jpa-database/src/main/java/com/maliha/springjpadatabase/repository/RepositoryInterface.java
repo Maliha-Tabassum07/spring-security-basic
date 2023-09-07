0fc27dcd20f7b65a3879c2bfc2e40abd9b47b955
MalihaBJIT <141900663+MalihaBJIT@users.noreply.github.com>
2023-09-07 15:05:06 +0600

    Delete spring-jpa-database directory

--- spring-jpa-database/src/main/java/com/maliha/springjpadatabase/repository/RepositoryInterface.java
+++
@@ -1,9 +0,0 @@
-package com.maliha.springjpadatabase.repository;
-
-import com.maliha.springjpadatabase.model.House;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositoryInterface extends JpaRepository<House,Integer> {
-}
--- spring-jpa-database/src/main/java/com/maliha/springjpadatabase/repository/WizardInterface.java
+++
@@ -1,8 +0,0 @@
-package com.maliha.springjpadatabase.repository;
-
-import com.maliha.springjpadatabase.model.Wizards;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WizardInterface extends JpaRepository<Wizards,Integer> {
-
-}
